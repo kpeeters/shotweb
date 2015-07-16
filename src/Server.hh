ff42b6b1b9bfe0b0e1b281c6b91cab262fb7f652
Kasper Peeters <kasper.peeters@fotoquirk.com>
2015-07-16 12:39:17 +0200

    Fixing authorisation.

--- src/Server.hh
+++ src/Server.hh
@@ -23,6 +23,33 @@ class Server {
 		void send_thumbnail(std::ostream& response, const std::string& fn, int max_size) const;
 		void send_photo(std::ostream& response, const Database::Photo&) const;
 
+		// Data structures for user -> event(s) authorisation mapping. These will 
+		// eventually come from a database but we have them hardcoded right now
+		// to get something up and running quickly.
+
+		class Authorisation {
+			public:
+				std::string      name;
+				std::string      password;
+				std::vector<int> events;
+		};
+		typedef std::string Token;
+
+		std::vector<Authorisation>     users;
+		std::map<Token, Authorisation> authorisations;
+
+		// Initialise the authorisations map from a disk database.
+		void        init_authorisations();
+
+		// Validate a user, return an access token if valid.
+		std::string validate_user(const std::string& user, const std::string& passwd);
+
+		// Determine if access to the given event is allowed with the given authorisation token.
+		bool        access_allowed(int event_id, const std::string& token) const;
+		
+		// Extract the authorisation token from the HTTP request header, if any.
+		std::string extract_token(std::shared_ptr<sw::Request> request);
+
 		Database db;
 		std::vector<Database::Event> events;
 };
